81bd4aa762d6cdc653af8844e37f2b096608b47b
Conor King <conor.king278@gmail.com>
2022-03-28 09:56:06 +0100

    Adding game files
    Added game files that seem relevant to our game project.

---
+++ Drop-Pod/scenes/scene_planet_level.cpp
@@ -0,0 +1,15 @@
+#include "scene_planet_level.h"
+#include "../game.h"
+#include <LevelSystem.h>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+
+void PlanetLevelScene::Load() { }
+
+void PlanetLevelScene::UnLoad() { }
+
+void PlanetLevelScene::Update(const double& dt) { }
+
+void PlanetLevelScene::Render() { }
---
+++ Drop-Pod/scenes/scene_spaceship_level.cpp
@@ -0,0 +1,15 @@
+#include "scene_spaceship_level.h"
+#include "../game.h"
+#include <LevelSystem.h>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+
+void SpaceshipLevelScene::Load() { }
+
+void SpaceshipLevelScene::UnLoad() { }
+
+void SpaceshipLevelScene::Update(const double& dt) { }
+
+void SpaceshipLevelScene::Render() { }
